43e8966b0debcdcac6bcc17286c5d5ae89504a4a
Leonardo <leobcontato@gmail.com>
2021-08-15 21:21:32 -0300

    0.1v

---
+++ src/app/components/tweet/tweet.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.scss'],
+})
+export class TweetComponent implements OnInit {
+
+  @Input() tweet: any;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
